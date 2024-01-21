d553cf76b4dfaf03b973c08c7b1c77a13cece4c8
shivams2003 <shivammun123@gmail.com>
2024-01-22 01:17:46 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./src/App.jsx";
+import App from "./App.jsx";
 import { ChakraProvider } from "@chakra-ui/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
