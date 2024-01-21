029a45c0134c9a570d028c18797b363bf47fbadb
shivams2003 <shivammun123@gmail.com>
2024-01-22 01:09:21 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// Replace 'urban-dictionary' with your actual repository name
+const repositoryName = 'urban-dictionary';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: `/${repositoryName}/`,
   plugins: [react()],
-})
+});
